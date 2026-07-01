fc216bcd4b534a16bfde79c63200f51571ec7ad2
vosco-cmd <diaroicoutoure13@gmail.com>
2026-07-01 09:10:40 +0000

    Add files via upload

---
+++ 006.py
@@ -0,0 +1,13 @@
+total_envisages = int(input())
+temp_minimal = int(input())
+temp_maximal = int(input())
+temp_relevee = int(input())
+pas_alarme = (temp_minimal <= temp_relevee) and (temp_relevee <= temp_maximal)
+while pas_alarme and total_envisages > 0:
+   print("Rien à signaler")
+   total_envisages = total_envisages - 1
+   if total_envisages > 0:
+      temp_relevee = int(input())
+      pas_alarme = (temp_minimal <= temp_relevee) and (temp_relevee <= temp_maximal)
+if not pas_alarme:
+   print("Alerte !!")
